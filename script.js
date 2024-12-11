07dcfe89a209ba10aedd8cfa3e6f6166c4e070e6
Michael <ndunwa240@gmail.com>
2024-12-11 19:03:25 +0100

    compressed image size

--- script.js
+++ script.js
@@ -11,10 +11,6 @@ let images = [
   { src: "image7.jpg", description: "Image 7 Description" },
   { src: "image8.jpg", description: "Image 8 Description" },
   { src: "image9.jpg", description: "Image 9 Description" },
-  { src: "image10.jpg", description: "Image 10 Description" },
-  { src: "image11.jpg", description: "Image 11 Description" },
-  { src: "image12.jpg", description: "Image 12 Description" },
-  { src: "image13.jpg", description: "Image 13 Description" },
 ];
 // current image index
 let currentIndex = 0;
@@ -36,11 +32,11 @@ function nextImage() {
 }
 
 function updateImage() {
-    const img = document.createElement('img')
- const imgwrapper =  document.querySelector(".image-container")
- imgwrapper.innerHTML = ''
- img.src = images[currentIndex].src
-imgwrapper.appendChild(img)
+  const img = document.createElement("img");
+  const imgwrapper = document.querySelector(".image-container");
+  imgwrapper.innerHTML = "";
+  img.src = images[currentIndex].src;
+  imgwrapper.appendChild(img);
   document.querySelector(".image-description").textContent = images[currentIndex].description;
 }
 // function updateImage() {
