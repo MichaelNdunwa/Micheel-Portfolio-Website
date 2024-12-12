b6fc617fb540a8b38029fe4cbd1453ad9ce429b3
Michael <ndunwa240@gmail.com>
2024-12-12 03:08:32 +0100

    changed the images path

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 // Image list
 let images = [
-  { src: "image1.jpg", description: "Happy me at TxE Summit 2023 Enugu." },
-  { src: "image2.jpg", description: "With a group of friends at Techflock 2022 Umuahi." },
-  { src: "image3.jpg", description: "Developing a mobile app at my space." },
-  { src: "image4.jpg", description: "My friends and I at GOtv event." },
-  { src: "image5.jpg", description: "Memory from my matriculation day." },
-  { src: "image6.jpg", description: "Studying for exams." },
-  { src: "image7.jpg", description: "Went to do solar installation with a Boss friend at Oyo." },
-  { src: "image8.jpg", description: "Enjoying the moment on a school tracktor." },
-  { src: "image9.jpg", description: "Participating in chemistry practical at my school lab, during my 100L days." },
+  { src: "https://utfs.io/f/3VnqAtHoXObw8hBi1YTkavuz71ReELfHTVQYmMwsy9PKiGFU", description: "Happy me at TxE Summit 2023 Enugu." },
+  { src: "https://utfs.io/f/3VnqAtHoXObwTbC1M6DdiJlRoIspn269QqF7mcwHZANKTjz4", description: "With a group of friends at Techflock 2022 Umuahi" },
+  { src: "https://utfs.io/f/3VnqAtHoXObwMxdfWoXxoWOXrfiytd3bBDGeaKmu8zh6NVRJ", description: "Developing a mobile app at my space." },
+  { src: "https://utfs.io/f/3VnqAtHoXObwMxdfWoXxoWOXrfiytd3bBDGeaKmu8zh6NVRJ", description: "My friends and I at GOtv event." },
+  { src: "https://utfs.io/f/3VnqAtHoXObwAi3Fpc7UuW8ZY2eJLRydgDq5XO6mctrpCBzk", description: "Memory from my matriculation day." },
+  { src: "https://utfs.io/f/3VnqAtHoXObwAi3Fpc7UuW8ZY2eJLRydgDq5XO6mctrpCBzk", description: "Studying for exams." },
+  { src: "https://utfs.io/f/3VnqAtHoXObwmjeGnn40Nsupc5SvL37faZoCQHyitwJdIUOG", description: "Went to do solar installation with a Boss friend at Oyo." },
+  { src: "https://utfs.io/f/3VnqAtHoXObwmjeGnn40Nsupc5SvL37faZoCQHyitwJdIUOG", description: "Enjoying the moment on a school tracktor." },
+  { src: "https://utfs.io/f/3VnqAtHoXObw7q4d2jfrvYq9w4n3hUOVsaQLzP6SeIdio1TE", description: "Participating in chemistry practical at my school lab, during my 100L days." },
 ];
 // current image index
 let currentIndex = 0;
