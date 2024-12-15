9a5f6e74cddb31bc3523b0df261e2670ec37085b
Michael <ndunwa240@gmail.com>
2024-12-15 13:10:44 +0100

    editted the webp extention

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 // Image list
   const images = [
-  { src: "./image1.jpg", description: "Happy me at TxE Summit 2023 Enugu." },
-  { src: "./image2.jpg", description: "With a group of friends at Techflock 2022 Umuahi" },
-  { src: "./image3.jpg", description: "Developing a mobile app at my space." },
-  { src: "./image4.jpg", description: "My friends and I at GOtv event." },
-  { src: "./image5.jpg", description: "Memory from my matriculation day." },
-  { src: "./image6.jpg", description: "Studying for exams." },
-  { src: "./image7.jpg", description: "Went to do solar installation with a Boss friend at Oyo." },
-  { src: "./image8.jpg", description: "Enjoying the moment on a school tracktor." },
-  { src: "./image9.jpg", description: "Participating in chemistry practical at my school lab, during my 100L days." },
+  { src: "./image1.webp", description: "Happy me at TxE Summit 2023 Enugu." },
+  { src: "./image2.webp", description: "With a group of friends at Techflock 2022 Umuahi" },
+  { src: "./image3.webp", description: "Developing a mobile app at my space." },
+  { src: "./image4.webp", description: "My friends and I at GOtv event." },
+  { src: "./image5.webp", description: "Memory from my matriculation day." },
+  { src: "./image6.webp", description: "Studying for exams." },
+  { src: "./image7.webp", description: "Went to do solar installation with a Boss friend at Oyo." },
+  { src: "./image8.webp", description: "Enjoying the moment on a school tracktor." },
+  { src: "./image9.webp", description: "Participating in chemistry practical at my school lab, during my 100L days." },
 ];
 // current image index
 let currentIndex = 0;
